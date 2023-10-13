a8200f61d7998307514bda67ac6180b4f9795ee6
sifatnabil <sifatnabil@gmail.com>
2023-10-13 18:18:08 +0600

    Fix the model

--- models/request_models.py
+++
@@ -1,5 +0,0 @@
-from pydantic import BaseModel
-from typing import List
-
-class Transactions(BaseModel):
-    transactions: List[dict] | dict
---
+++ models/transacations.py
@@ -0,0 +1,22 @@
+from typing import List
+from pydantic import BaseModel
+
+class Transaction(BaseModel):
+    transaction_id: str
+    date: str
+    amount: float
+    merchant: str
+    category: List[str]
+    location: dict
+    payment_method: str
+
+    def to_dict(self):
+        return {
+            "transaction_id": self.transaction_id,
+            "date": self.date,
+            "amount": self.amount,
+            "merchant": self.merchant,
+            "category": self.category,
+            "location": self.location,
+            "payment_method": self.payment_method
+        }
