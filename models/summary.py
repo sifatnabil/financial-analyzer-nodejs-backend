34f0dc0491d3b7802a51ef57f978e5c0b5ee96aa
sifatnabil <sifatnabil@gmail.com>
2023-10-15 13:35:57 +0600

    fix nullable fields in the summary

--- models/summary.py
+++ models/summary.py
@@ -6,18 +6,18 @@ class Summary(BaseModel):
     status: str
     totalSpending: float
     totalEarning: float
-    total_spending_percentage: float
-    cautionDate: datetime
-    metric: float
-    maxTransaction: float
-    modeMerchant: str
+    totalSpendingPercentage: float
+    cautionDate: datetime | None = None
+    metric: float | None = None
+    maxTransaction: float | None = None
+    modeMerchant: str | None = None
 
     def to_dict(self):
         return {
             "status": self.status,
             "totalSpending": self.totalSpending,
             "totalEarning": self.totalEarning,
-            "total_spending_percentage": self.total_spending_percentage,
+            "total_spending_percentage": self.totalSpendingPercentage,
             "cautionDate": self.cautionDate,
             "metric": self.metric,
             "maxTransaction": self.maxTransaction,
