dc517fa1f33201440cde6445ea13b6a71860e05f
sifatnabil <sifatnabil@gmail.com>
2023-10-17 15:28:33 +0600

    create get prompts function

--- utils/helper.py
+++ utils/helper.py
@@ -1,5 +1,6 @@
 from pymongo import MongoClient
 from dotenv import dotenv_values
+from bson import ObjectId
 
 def get_collection(db_name="TransactionDB", collection_name="transactions"):
    # Fetch the configuration settings
@@ -13,3 +14,18 @@ def get_collection(db_name="TransactionDB", collection_name="transactions"):
     
     # Get the database collection instance
     return client[db_name][collection_name]
+
+
+def get_prompts(ids):
+   # Get the MongoDB collection
+   collection = get_collection(collection_name="prompts")
+
+   # Retrieve the paragraphs from MongoDB
+   paragraphs = []
+   for paragraph_id in ids:
+      paragraph = collection.find_one({"_id": ObjectId(paragraph_id)})
+      if paragraph:
+         paragraphs.append(paragraph["text"])
+
+   # Return the paragraphs in a JSON response
+   return paragraphs
